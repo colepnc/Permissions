3e7ff536ad7f059a47974a7333d27bd8e889b66b
colepnc <cole@pointandclicktulsa.com>
2017-03-03 11:55:34 -0600

    Working
    Working script for a three site envirnment where you want all sites to
    have read/execute, but only the site itself has Full Control of it's own
    share.

--- Multi-site Permissions.ps1
+++ Multi-site Permissions.ps1
@@ -33,36 +33,33 @@ Take ownership (WO)
 $ErrorActionPreference = "SilentlyContinue"
 Stop-Transcript | out-null
 $ErrorActionPreference = "Continue"
-Start-Transcript -path C:\Users\user\MappedDriveCleanup.txt -append
+Start-Transcript -path C:\Users\Cole\MappedDriveCleanupMulti-Site.txt -append
 
-$Folder = "\\server\shareroot"
+$Folder = "\\server\share"
 $Subfolders = Get-ChildItem $Folder | 
               Where-Object {$_.PSIsContainer} | 
               Select-Object -ExpandProperty FullName
-$UserGroup = "Users"
-$SUGroup = "SuperUsers"
+$UserGroup = "remote1" # remote-site group
+$UserGroup2 = "remote2" # remote-site2 group
+$SUGroup = "local" # Site-local group
 
-# Phase 1 Sets the owner to the superuser group you intend to use to manage the drive
+# Phase 1 Sets the owner to the site-local user group
 Write-Host "Phase 1: Giving ownership to $SUGroup"
 icacls "$Folder" /setowner $SUGroup /T /C 
 
 # Phase 2 Reset permissions on all files
 Write-Host "Phase 2: Resetting possibly messy permissions, disabling inheritance on $Folder, and removing defualt usergroup"
 icacls "$Folder" /reset /T /C 
-# Disable inheritance on the root of the mapped drive
-icacls "$Folder" /inheritance:d /C 
-# Disable inheritance on all subfolders of root folder (prevents duplicate acl entries)
-foreach ($Subfolder in $Subfolders) {icacls "$Subfolder" /inheritance:d /C}
 # Remove "Users" from the defualt permissions
 icacls "$Folder" /remove "Authenticated Users" /T /C 
 
-# Phase 3 grant $UserGroup the appropriate permissions (deny modify) to the root of the mapped drive
+# Phase 3 grant $UserGroup & $UserGroup2 the appropriate permissions (deny modify) to the root of the mapped drive
 Write-Host "Phase 3: Granting permissions to $UserGroup and $SUGroup"
-icacls "$Folder" /grant ("$UserGroup" + ':(OI)(CI)(X,RD,RA,REA,RC)') /T /C 
-# grant $UserGroup the appropriate permissions to the subdirectories (everything but changing permissions/ownership)
-foreach ($Subfolder in $Subfolders) {icacls "$Subfolder" /grant ("$UserGroup" + ':(OI)(CI)(NP)(IO)(WD,AD,WA,WEA,DC,D)') /T /C}
-# grant $SUGroup full control
-icacls "$Folder" /grant ("$SUGroup" + ':(F)')
+icacls "$Folder" /grant ("$UserGroup" + ':(OI)(CI)(X,RD,RA,REA,RC)') /C
+icacls "$Folder" /grant ("$UserGroup2" + ':(OI)(CI)(X,RD,RA,REA,RC)') /C 
+# grant $SUGroup full control and domain admins
+icacls "$Folder" /grant ("$SUGroup" + ':(OI)(CI)(F)') /C
+icacls "$Folder" /grant ("Domain Admins" + ':(OI)(CI)(F)') /C
 
 # End log file
 Stop-Transcript
