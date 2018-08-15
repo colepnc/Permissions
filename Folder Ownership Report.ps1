e8b5e3a625fe7ad9420925734a9837581595bc5d
colepnc <cole@pointandclicktulsa.com>
2018-08-15 14:15:48 -0500

    Get ownership
    Gets the ownership of all subfolders recursively, and spits it out into a CSV. The Microsoft.PowerShell.Core\FileSystem:: is annoying, but it can be filtered out with a simple task in Excel.

---
+++ Folder Ownership Report.ps1
@@ -0,0 +1,10 @@
+﻿# The purpose of this script is to report on folder ownership for all folders/subfolders
+
+# $Folders is the root of the mapped drive share, $Subfolders will recurse all files/folders below it
+
+$Folder = "C:\Share"
+$Subfolders = Get-ChildItem $Folder -Recurse | 
+              Where-Object {$_.PSIsContainer} | 
+              Select-Object -ExpandProperty FullName
+
+foreach ($Subfolder in $Subfolders) {Get-Acl "$Subfolder" | Select-Object Path,Owner | Export-CSV c:\admin\owners.csv -Append}
